01ee8b0d84e89cb2fbc9b57b1795061570822698
razimasri <46912821+razimasri@users.noreply.github.com>
2021-10-09 16:10:03 +0700

    minor

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -17,6 +17,6 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.LookAt(player);
+        transform.LookAt(player, Vector3.forward);
     }
 }
